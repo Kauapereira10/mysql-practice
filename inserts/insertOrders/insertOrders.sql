f84243f5f69b933d458c2b4053105254b917213a
Kauã Pereira <borgeskaua1210@gmail.com>
2026-03-13 14:31:15 -0300

    adicionar inserts de pedidos vinculados aos usuários

---
+++ inserts/insertOrders/insertOrders.sql
@@ -0,0 +1,9 @@
+INSERT INTO sistema_de_pedidos.orders
+(status, total_amount, user_id)
+VALUES
+('PENDING', 379.80, 1),
+('COMPLETED', 379.80, 1),
+('COMPLETED', 1259.80, 2),
+('PENDING', 249.90, 3),
+('COMPLETED', 1899.90, 4),
+('PENDING', 409.70, 5);
