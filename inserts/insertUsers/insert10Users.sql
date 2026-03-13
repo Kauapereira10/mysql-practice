7bf99cfe1474cdd3be71c89b04cc8b38596a394b
Kauã Pereira <borgeskaua1210@gmail.com>
2026-03-13 14:30:06 -0300

    adicionar inserts de usuários para prática no banco sistema_de_pedidos

---
+++ inserts/insertUsers/insert10Users.sql
@@ -0,0 +1,13 @@
+INSERT INTO sistema_de_pedidos.users 
+(first_name, last_name, email, password_hash)
+VALUES
+('Kaua', 'Pereira', 'kaua.pereira@email.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd38'),
+('Mariana', 'Silva', 'mariana.silva@email.com', '2bb80d537b1da3e38bd30361aa855686bde0ba8b8'),
+('Lucas', 'Oliveira', 'lucas.oliveira@email.com', '8d969eef6ecad3c29a3a629280e686cff8fab6'),
+('Ana', 'Souza', 'ana.souza@email.com', '4e07408562bedb8b60ce05c1decfe3ad16b722309'),
+('Pedro', 'Santos', 'pedro.santos@email.com', 'ef92b778bafe771e89245b89ecbc08a44a4e166c'),
+('Juliana', 'Costa', 'juliana.costa@email.com', '6cb75f652a9b52798eb6cf2201057c73e067'),
+('Gabriel', 'Almeida', 'gabriel.almeida@email.com', '3ac674216f3e15c761ee1a5e255f067953623c8b'),
+('Beatriz', 'Rocha', 'beatriz.rocha@email.com', '7c6a180b36896a0a8c02787eeafb0e4c'),
+('Rafael', 'Ferreira', 'rafael.ferreira@email.com', '9d5ed678fe57bcca610140957afab571'),
+('Camila', 'Gomes', 'camila.gomes@email.com', '0d107d09f5bbe40cade3de5c71e9e9b7');
